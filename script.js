82015b00a8682c4f34345b51f2d5f0186889ba52
pinzdesign <tyrael2101@hotmail.com>
2023-08-28 13:05:40 +0200

    message

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // skriv din kode her.
+const roses = document.querySelectorAll("main img");
+//console.log(roses);
 
 // vælg alle blomster og add en click eventlistener til dem.
+roses.forEach((rose) => rose.addEventListener("click", rotateRose));
 
 // lav en funktion der tilføjer class'en "pluk" til den blomst der bliver klikket på.
+function rotateRose() {
+    this.classList.toggle("pluk");
+}
